3edc8f6dda8c04ef8c10c2deb4fcfd6a76199319
jhow133 <146762188+jhow133@users.noreply.github.com>
2024-10-23 21:53:21 -0300

    projeto

---
+++ app.js
@@ -0,0 +1,27 @@
+function criacartao(categoria,pergunta,resposta){
+let container=document.getElementByld('container')
+let cartao= document.createElement('article')
+cartao.className='cartao'
+
+cartao.innerHTML=
+
+<div class="cartao_conteudo">
+<h3>${categoria}</h3>
+<div class="cartao_conteudo-pergunta">
+    <p>4{pergunta}</p>
+</div>
+<div class="cartao_conteudo_resposta">
+    <p>${resposta}</p>
+</div>
+</div>
+
+let respostaEstaVisivel= false
+
+function viraCartao(){
+respostaEstaVisivel= !respostaEstaVisivel
+cartao.classList.toggle('active,respostaEstaVisivel')
+}
+cartao.addEventListener('click', viraCartao)
+container.appendchild(cartao)
+
+}
---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criacartao(
+'Programação',
+'O que é CSS.',
+'O CSS é uma linguagem de estilização.'
+)
+criacartao(
+'Matemática',
+' Quantos é 5x3+4x7.',
+'O resultado é 43.'
+)
+criacartao(
+'Matemática',
+'Quantos é 12 elevado ao quadrado.',
+'O resultado é 144.'
+)
+criacartao( 
+'Matemática',
+'Qual é a raiz quadrada de 225.',
+'O resultado é 15.'
+)
+criacartao(
+'História',
+'O que foi o Tratado de Versalhes. ',
+'O Tratado de Versalhes encerrou formalmente a Primeira Guerra Mundial.'
+)
+
+
+
+
